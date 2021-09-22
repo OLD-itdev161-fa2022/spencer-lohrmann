682080103cd74d2c83cf79dfd698db8f95db41ef
Spencer Lohrmann <lohrmans@gmatc.matc.edu>
2021-09-21 19:27:50 -0500

    Created User Registration API Endpoint

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 import express from 'express';
-import { connect } from 'mongoose';
 import connectDatabase from './config/db';
 
+//Initialize express application
 const app = express();
 
+//Connect database
 connectDatabase();
 
+//Configure Middleware
+app.use(express.json({ extended: false}));
+
+// API endpoints
+/*
+* @route GET /
+* @desc Test endpoint
+*/ 
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+// API endpoints
+/*
+* @route POST api/users
+* @desc Register user
+*/ 
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
